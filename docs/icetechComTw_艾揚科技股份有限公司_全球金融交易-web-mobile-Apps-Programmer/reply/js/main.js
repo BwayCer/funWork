6cc5a9aad455a5526f28935caffb7a96af79e731
BwayCer <bwaycer@gmail.com>
2018-04-19 16:07:28 +0800

    增補 移除 JQ 依賴

--- docs/icetechComTw_艾揚科技股份有限公司_全球金融交易-web-mobile-Apps-Programmer/reply/js/main.js
+++ docs/icetechComTw_艾揚科技股份有限公司_全球金融交易-web-mobile-Apps-Programmer/reply/js/main.js
@@ -1,6 +1,39 @@
-$(document).ready(function(){
-	var urlData1 = "data/data1.json";
-	var urlData2 = "data/data2.json";
-	var urlData3 = "data/data3.json";
+'use strict';
 
-});
+// 當文件載入時觸發事件
+void function ( getClaDataATable, fnReadyAction ) {
+    var idxState;
+    var stateList = [ 'loading', 'interactive', 'complete' ];
+
+    idxState = stateList.indexOf( document.readyState );
+
+    if ( idxState > 0 ) fnReadyAction( getClaDataATable() );
+    else {
+        document.addEventListener( 'DOMContentLoaded', function tmp( evt ) {
+            document.removeEventListener( 'DOMContentLoaded', tmp, false );
+            fnReadyAction( getClaDataATable() );
+        }, false );
+    }
+}(
+function () {
+    function dataATable ( helTable, helUsage ) {
+        this._helTable = helTable;
+        this._helUsage = helUsage;
+    }
+
+    dataATable.prototype._dataFileList = [
+        './data/data1.json',
+        './data/data2.json',
+        './data/data3.json',
+    ];
+
+    return dataATable;
+},
+function ( claDataATable ) {
+    var insDataATable = new claDataATable(
+        document.querySelector("table#show"),
+        document.querySelector( '.usuage' )
+    );
+}
+);
+
