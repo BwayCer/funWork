68a21082625e9420a7534b12cbed3664c9a60ee2
BwayCer <bwaycer@gmail.com>
2019-07-14 18:23:58 +0800

    增補 預測最大對半切唯一值數組的長度

--- docs/marsblockchainCom_火星區塊鏈有限公司_前端軟體工程師/reply/proof/countCutHalfLoop/countCutHalfLoop.go
+++ docs/marsblockchainCom_火星區塊鏈有限公司_前端軟體工程師/reply/proof/countCutHalfLoop/countCutHalfLoop.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -25,22 +26,21 @@ func main() {
 		return
 	}
 
+	maxUniqueCutHalfLength := GetMaxUniqueCutHalfLength(theValue)
+
 	if len(argv) > 1 && argv[1] != "" {
 		theQuantity, _ = strconv.Atoi(argv[1])
 	} else {
-		// TODO 最大可被裁切數
-		// 以 "除以 2 仍不為 0 的個數 + 1" 的值暫代，
-		// 因為 make([]int, leng, capa) 的 capa 的值無法過大
-		theQuantity = len(strconv.FormatInt(int64(theValue), 2)) + 1
+		theQuantity = maxUniqueCutHalfLength
 	}
 
 	switch theValue {
 	case 0:
-		fmt.Println("共計被裁切個數為： 0")
+		fmt.Println("共計被裁切個數為：     0")
 	case 1, 2:
 		fmt.Printf(" %d = %d\n", theValue, theValue)
 		fmt.Printf("%#v\n", []int{theValue})
-		fmt.Printf("共計被裁切個數為： %d\n", theValue)
+		fmt.Printf("共計被裁切個數為：     %d\n", theValue)
 	default:
 		listIdx := 0
 		list := make([]int, 0, theQuantity)
@@ -70,11 +70,31 @@ func main() {
 
 		list = append(list, tmpNewVal)
 		fmt.Printf("%#v\n", list)
-		fmt.Printf("共計被裁切個數為： %d\n", listIdx+1)
+		fmt.Printf("共計被裁切個數為：     %d\n", listIdx+1)
 	}
+
+	fmt.Printf("預計最大可被裁切個數： %d\n", GetMaxUniqueCutHalfLength(theValue))
 }
 
 const (
 	BitOfPlatform = 32 << (^uint(0) >> 63)
 	NumberMaxInt  = 1<<(BitOfPlatform-1) - 1
 )
+
+// 返回最大對半切唯一值數組長度。
+func GetMaxUniqueCutHalfLength(value int) int {
+	if value < 1 {
+		return 0
+	} else if value < 3 {
+		return 1
+	} else {
+		var bitLength int
+		valueBits := strconv.FormatInt(int64(value), 2)
+		bitLength = len(valueBits)
+		all1Bits := strings.Repeat("1", bitLength)
+		if valueBits != all1Bits {
+			bitLength--
+		}
+		return 2 + (bitLength - 2)
+	}
+}
